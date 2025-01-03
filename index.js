072babc12c4e0b6b5fd70e838afbb6a9ee236e63
Kiesha Maria <kiesha.maria@kalvium.community>
2025-01-03 11:57:48 +0530

    assignment completed

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
+
+require('dotenv').config();
 const express = require('express');
 const { resolve } = require('path');
 
 const app = express();
+const mongoose=require('mongoose');
 const port = 3010;
 
-app.use(express.static('static'));
+const connectstring=process.env.MONGO_URI;
+mongoose.connect(connectstring)
+    .then(() => console.log('Connected to database'))
+    .catch((error) => {
+        console.error(`Error connecting to database: ${error.message}`);
+        
+    });
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
-});
+    app.get('/', (req, res) => {
+      res.sendFile(resolve(__dirname, 'pages/index.html'));
+    });
 
+
+    console.log(process.env.MONGO_URI);
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
